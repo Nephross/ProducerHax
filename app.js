6deaecb24c05a53777e37f2303d486e4ca337cfb
PeterD-J <peter.d.wiredelta@gmail.com>
2017-05-26 04:59:19 +0200

    Converted to angular and got started on some of the smaller frontend jobspart 2

--- app.js
+++ app.js
@@ -9,52 +9,51 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var about = require('./routes/about');
-var blog = require('./routes/blog');
-// var podcast = require('./routes/podcast');
-// var users = require('./routes/users');
-
+let api = require('./api/api');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'vash');
-app.set('x-powered-by', false); //Disables the http header value of x-powered-by
+
+app.set('x-powered-by', false); // Disables the http header value of x-powered-by
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '/app/public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(csrf({ cookie: true })); //CRSF protection
-app.use(session({secret: 'ssshhhhh'})); //Session initialized
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(csrf({ cookie: true })); // CRSF protection
+app.use(session({
+  secret: 'ssshhhhh',
+  resave: false,
+  saveUninitialized: false // To reduce storrage usage
+})); // Session initialized
+app.use(express.static(path.join(__dirname, '/app/public/')));
+app.use(express.static(path.join(__dirname, '/app/')));
 
-app.use('/', index);
-app.use('/about', about);
-app.use('/blog', blog);
-//app.use('/podcast', podcast);
-//app.use('/users', users);
+// routes
+app.use('/api', api);
+
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+  res.sendFile(path.join(__dirname, '/app/public/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   if (err.code !== 'EBADCSRFTOKEN') {
-  	next(err);
+    next(err);
   }
   // handle CSRF token errors here
-  res.status(403)
-  res.send('form tampered with')
+  res.status(403);
+  return res.send('form tampered with');
 });
 // error handler
 app.use(function(err, req, res, next) {
@@ -64,7 +63,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  return res.render('error');
 });
 
 module.exports = app;
